1ca80f3a13fe6885bea8b8c5334176326d5944a1
Harry Harper <146091658+SlothzDoCode@users.noreply.github.com>
2025-08-09 17:26:34 +0100

    Update setup.py
    added in label explination

--- angus tkinter/setup.py
+++ angus tkinter/setup.py
@@ -3,4 +3,16 @@ import tkinter as tk #easier to call if shorthanded
 
 display = tk.Tk() #creates a location for stuff to be placed
 
+#!labels
+#labels are the tkinter equivelent of textboxes
+
+label = tk.Label(bg="", #same as in config
+                 fg="", #either use fg or foreground and a colour or hex code to set the text colour
+                 text="", #self explanitory
+                 width="", #the width of the label
+                 height="", #the height of the label
+                 font=("",""), #the first set of "" is the font name the second "" is for the font size
+                 )
+
 display.mainloop() #will create the window and all items on the display when script is run
+
