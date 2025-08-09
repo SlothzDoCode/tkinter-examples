1c4aa1cd57d099b887d8d3389f622ea575841fb9
Harry Harper <146091658+SlothzDoCode@users.noreply.github.com>
2025-08-09 17:16:27 +0100

    Update setup.py
    window creation

---
+++ angus tkinter/setup.py
@@ -0,0 +1,6 @@
+from tkinter import * #for some reason you need to do this to get all parts that you will need not sure why
+import tkinter as tk #easier to call if shorthanded
+
+display = tk.Tk() #creates a location for stuff to be placed
+
+display.mainloop() #will create the window and all items on the display when script is run
